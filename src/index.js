4de51d5434c8964ec27a2b83e68e319d0d874638
SanjanaBhojwani <99994728+SanjanaBhojwani@users.noreply.github.com>
2023-05-26 17:38:50 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+var React = require("react");
+var ReactDom = require("react-dom");
+ReactDom.render(
+  <div>
+    <h1>Task1</h1>
+    <ul>
+      <li>pizza</li>
+      <li>burger</li>
+      <li>momo</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
